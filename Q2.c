cd2027650fc35b04d35ffd09dbdec63ad7b31a0c
Saran J A <riofeddricksaran@gmail.com>
2025-08-28 23:20:29 -0700

    Add files via upload

---
+++ Q2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+	char word [10];
+	printf("Enter a word :");
+	scanf("%s",word);
+	printf("%s is entered word",word);
+	return 0;
+}
+     
---
+++ Q5.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+	float frac;
+	printf("Enter a fractional number :");
+	scanf("%f",&frac);
+	printf("%f is entered fractional number",frac);
+	return 0;
+}
+     
