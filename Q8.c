ff29a99140d84064894895a9ecfd9fa5dbdcab9e
Saran J A <riofeddricksaran@gmail.com>
2025-08-30 21:49:46 +0530

    Update Q8.c

--- Q8.c
+++ Q8.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
     int num;
-    printf("Enter a integer number :");
+    printf("Enter an integer number :");
     scanf("%d",&num);
     printf("%o is Octal of %d",num,num);
     return 0;
