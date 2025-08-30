2cfcca478170a78e927f60ab5c478d6aab17ad2d
Saran J A <riofeddricksaran@gmail.com>
2025-08-30 21:49:12 +0530

    Update Q4.c

--- Q4.c
+++ Q4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
 	int integer;
-	printf("Enter a integer number :");
+	printf("Enter an integer number :");
 	scanf("%d",&integer);
 	printf("%d is entered integer number",integer);
 	return 0;
