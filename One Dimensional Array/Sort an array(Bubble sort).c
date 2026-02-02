ff282c727f4b0f0b302f612c1cd4f3fe0f41c2e6
Saran J A <riofeddricksaran@gmail.com>
2026-02-02 18:10:29 +0530

    Update Sort an array(Bubble sort).c

--- One Dimensional Array/Sort an array(Bubble sort).c
+++ One Dimensional Array/Sort an array(Bubble sort).c
@@ -7,7 +7,7 @@ int main()
 	for(int i=0; i<n; i++) {
 		scanf("%d",&arr[i]);
 	}
-	for(int i=0; i<n; i++){
+	for(int i=0; i<n-1; i++){
 	        for(int j=0;j<n-i-1;j++)
 	         {
 	            if(arr[j]>arr[j+1]){
@@ -16,4 +16,5 @@ int main()
 	                arr[j+1]=temp;}}}
 	   for(int i=0; i<n; i++) {
 		printf("%d ",arr[i]);}
+
 }
