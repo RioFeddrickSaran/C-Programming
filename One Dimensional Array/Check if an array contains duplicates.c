82b948be2b522ff6789087a9201215293e48d71b
Saran J A <riofeddricksaran@gmail.com>
2026-02-03 11:25:16 +0530

    Add files via upload

--- One Dimensional Array/Check if an array contains duplicates.c
+++ One Dimensional Array/Check if an array contains duplicates.c
@@ -1,22 +1,18 @@
 #include <stdio.h>
-int main()
-{
-	int n,count=0;
-	scanf("%d",&n);
-	int arr[n];
-	for(int i=0; i<n; i++) {
-		scanf("%d",&arr[i]);
-	}
-	for(int i=0; i<n; i++){
-	    for(int j=i+1;j<n-1;j++){
-	        if(arr[i]==arr[j]){
-	            count=1;
-	            break;
-	        }
-	    }
-	}
-	if(count==0)
-	printf("No Duplicates");
-	else
-	printf("Duplicates are present");
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for(int i=0; i<n; i++) {
+        scanf("%d", &arr[i]); }
+    for(int i=0; i<n; i++) {
+        for(int j=i+1; j<n; j++) {
+            if(arr[i] == arr[j]) {
+                for(int k=j; k<n-1; k++) {
+                    arr[k] = arr[k+1]; }
+                n--;   
+                j--;   }  }  }
+    for(int i=0; i<n; i++) {
+        printf("%d ", arr[i]);}
 }
