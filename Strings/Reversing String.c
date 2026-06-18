980d5a979d61434737bb9bb8b9f42979049e5815
Saran J A <riofeddricksaran@gmail.com>
2026-06-18 15:07:24 +0530

    Update Reversing String.c

--- Strings/Reversing String.c
+++ Strings/Reversing String.c
@@ -4,9 +4,8 @@ int main() {
     char s[100];
     fgets(s, sizeof(s), stdin);
     int len = strlen(s);
-    for (int i = 0; i < len / 2; i++) {
+    for (int i=0;i<len/2;i++) {
         char temp = s[i];
-        s[i] = s[len - 1 - i];
-        s[len - 1 - i] = temp;}
-    printf("%s",s);
-}
+        s[i] = s[len-1-i];
+        s[len-1-i] =temp;}
+    printf("%s",s);}
